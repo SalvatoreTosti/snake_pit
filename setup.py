e4f837e0187d85f22fb95859feb87308d2f294c1
Salvatore Tosti <saltosti@gmail.com>
2019-05-08 09:52:58 -0700

    Updates setup to be much more descriptive

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-setup(name='snake_pit',
-      version='0.1',
-      description='Customizable multi-processing system.',
-      url='https://github.com/SalvatoreTosti/snake_pit',
-      author='Salvatore Tosti',
-      author_email='',
-      license='MIT',
-      packages=['snake_pit'],
-      zip_safe=False)
+from distutils.core import setup
+setup(
+  name = 'snakePit',         # How you named your package folder (MyLib)
+  packages = ['snakePit'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        
+  description = 'Customizable multi-processing system.',
+  author = 'Salvatore Tosti',                   
+  author_email = 'your.email@domain.com',
+  url = 'https://github.com/SalvatoreTosti/snake_pit',   
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    
+  keywords = ['Multiprocessing', 'multi', 'processing', 'multithreading', 'threading'],   
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',  
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+)
